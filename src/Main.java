e0c93f0e71339c9ab40e34ef9320ca43ab71a379
lvukasinovic <61170058+lvukasinovic@users.noreply.github.com>
2020-05-09 20:12:04 -0400

    Update and rename Main.java to src/Main.java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/* Liam Vukasinovic
+ * May 31 2019
+ * This is the file that you start the game from.
+ */
+
+
+public class Main {
+  
+  public static void main(String[] args) { 
+    Minesweeper game = new Minesweeper(); //Starts the game
+  }
+  
+}
